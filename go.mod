3910062c3fc0bdb79da44d7523ceb58a0614d899
eidyz <eidyz819@gmail.com>
2020-03-16 23:53:24 +0100

    :tada: init

---
+++ go.mod
@@ -0,0 +1,5 @@
+module github.com/eidyz/qrgen
+
+go 1.13
+
+require github.com/yeqown/go-qrcode v1.2.1
---
+++ main.go
@@ -0,0 +1,18 @@
+package main
+
+import (
+	"fmt"
+	qrcode "github.com/yeqown/go-qrcode"
+)
+
+func main(){
+	qrc, err := qrcode.New("https://github.com/yeqown/go-qrcode")
+	if err != nil {
+		fmt.Printf("could not generate QRCode: %v", err)
+	}
+
+	// save file
+	if err := qrc.Save("../testdata/repo-qrcode.jpeg"); err != nil {
+		fmt.Printf("could not save image: %v", err)
+	}
+}
