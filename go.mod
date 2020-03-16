d66346bfdcf6a24ae1deb47e5cd0fed0533c7c43
eidyz <eidyz819@gmail.com>
2020-03-17 00:44:24 +0100

    :sparkles: basic

--- go.mod
+++ go.mod
@@ -2,4 +2,7 @@ module github.com/eidyz/qrgen
 
 go 1.13
 
-require github.com/yeqown/go-qrcode v1.2.1
+require (
+	github.com/skip2/go-qrcode v0.0.0-20191027152451-9434209cb086
+	github.com/yeqown/go-qrcode v1.2.1
+)
--- main.go
+++ main.go
@@ -1,18 +1,91 @@
 package main
 
 import (
+	"bufio"
 	"fmt"
-	qrcode "github.com/yeqown/go-qrcode"
+	"io/ioutil"
+	"log"
+	"os"
+	"strconv"
+	"strings"
+
+	qrcode "github.com/skip2/go-qrcode"
 )
 
-func main(){
-	qrc, err := qrcode.New("https://github.com/yeqown/go-qrcode")
-	if err != nil {
-		fmt.Printf("could not generate QRCode: %v", err)
+const OUTPUT_DIR = "qr-codes"
+const INPUT_DIR = "payloads"
+
+func CreateDirIfNotExist(dir string) (e error) {
+	if _, err := os.Stat(dir); os.IsNotExist(err) {
+		err = os.MkdirAll(dir, 0755)
+		if err != nil {
+			e = err
+		}
 	}
 
-	// save file
-	if err := qrc.Save("../testdata/repo-qrcode.jpeg"); err != nil {
-		fmt.Printf("could not save image: %v", err)
+	return
+}
+
+func ListDir(dir string) (fileNames []string, e error) {
+	files, err := ioutil.ReadDir(dir)
+	if err != nil {
+		e = err
 	}
+
+	for _, f := range files {
+		fileNames = append(fileNames, f.Name())
+	}
+
+	return
+}
+
+func ReadFile(path string) (fileTextLines []string, e error) {
+	readFile, err := os.Open(path)
+
+	if err != nil {
+		e = err
+	}
+
+	fileScanner := bufio.NewScanner(readFile)
+	fileScanner.Split(bufio.ScanLines)
+
+	for fileScanner.Scan() {
+		fileTextLines = append(fileTextLines, fileScanner.Text())
+	}
+
+	readFile.Close()
+
+	return
+}
+
+func main() {
+	err := CreateDirIfNotExist(OUTPUT_DIR)
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	fileNames, err := ListDir("./payloads")
+	if err != nil {
+		log.Fatal(err)
+	}
+
+	for _, file := range fileNames {
+		fileLines, err := ReadFile(INPUT_DIR + "/" + file)
+		if err != nil {
+			log.Fatal(err)
+		}
+
+		for lineIndex, line := range fileLines {
+			fileNoExt := strings.Replace(file, ".txt", "", -1)
+			sLineIndex := strconv.Itoa(lineIndex)
+			outputFilePath := OUTPUT_DIR + "/" + fileNoExt + "-" + sLineIndex + ".png"
+
+			err := qrcode.WriteFile(line, qrcode.Medium, 256, outputFilePath)
+			if err != nil {
+				fmt.Println("Failed to generate QR Code from", file, "line:", lineIndex, "(", err, ")")
+			}
+		}
+	}
+
+	fmt.Println("Done generating QR Codes")
 }
